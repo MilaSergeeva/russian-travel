fa5855a2f7f874c00d09da07ecaeaaf0d21e9038
Mila Sergeeva <buka4a@gmail.com>
2021-02-04 12:24:18 +0100

    translation added

---
+++ translate.js
@@ -0,0 +1,29 @@
+const en_content = {
+  title1: "Traveling through Russia",
+  parag: "The real country is not in the news, but here.",
+  shelf_title: "What didn`t we see there?",
+  shelf_parag:
+    'According to VTsIOM polls, 95% of Russians dream of going somewhere, but only 36% plan to spend their holidays in their home country. Like, what have we not seen here at home? In fact, Russia is a whole universe with the gentle sea of ​​the south, dense Sayan forests and the harsh ice of the Putorana plateau. And you can also see all these beauties without millions of rubles on your account, a foreign passport and many hours of flights. As, for example, Vera Bashmakova - a brave young mother who took three children in her arms, put them in her "Lada" and drove 20 thousand kilometers through her native country. We have selected and described some interesting places worthy of your vacation.',
+  numbers: {
+    "Time zones": 11,
+    "Natural heritage sites of UNESCO": 12,
+    "UNESCO cultural heritage sites": 16,
+    "Nature reserves": 105,
+    Airports: 241,
+  },
+  places: {
+    "The Curonian Spit ":
+      "The foundations of the spit were created about 15,000 BCE as the receding glaciers left the Baltic Sea behind, and the sand dunes of the Curonian built on the glacial moraine. The actions of sea and wind build large dunes, which average 35 meters on the spit. The area is one of high biodiversity due to the many different ecological communities in close proximity to each other: beach, dune ridge, wetlands of various types, meadows and forests. The Curonian Spit is the second longest spit in the world, after the 110-km long Arabat Spit in the Sea of Azov. The park follows the spit from the Sambian Peninsula in the south to the border with Lithuania about 40 km north; its width varies from 0.4 to 4 km. The water in the lagoon averages 3.7 meters in depth, and the water level of the lagoon is about 12 cm above that of the Baltic.",
+    "Kola Peninsula":
+      "The peninsula is located in the far northwest of Russia, almost completely inside the Arctic Circle and is bordered by the Barents Sea in the north and the White Sea in the east and southeast.[1] Geologically, the peninsula occupies the northeastern edge of the Baltic Shield. The western border of the peninsula stretches along the meridian from the Kola Bay through the valley of the Kola River, Lake Imandra, and the Niva River to the Kandalaksha Gulf, although some sources push it all the way west to Russia`s border with Finland.",
+    Altai:
+      "Altai Krai has rolling foothills, grasslands, lakes, rivers, and mountains. The climate is severe with long cold dry winters and hot, usually dry summers. The region`s main waterway is the Ob River. The Biya and Katun Rivers are also important. The biggest lakes are Lake Kulundinskoye, Lake Kuchukskoye, and Lake Mikhaylovskoye. Altai Krai has rich natural resources, including lumber, as well as significant mineral reserves. These include the nonferrous metals lead, manganese, tungsten, molybdenum, bauxite, and gold, as well as iron ore. Forests cover about 60,000 km2 (23,000 sq mi) of the krai`s land. See also Geography of South-Central Siberia. This region of Siberia is extremely important due to its biodiversity, an area of over 1.6 million hectares (16,000 square kilometres; 6,200 square miles) is recognised by Unesco as a world heritage site. The area is home to animals considered rare, including the endangered snow leopard.",
+    "Winter Baikal":
+      "Winter Baikal is a real fairy tale land – transparent ice, bright sunshine from which you can even get a tan, and at the same time freezing temperatures. Each year more and more tourists are visiting Baikal during this period. The ice of the frozen Lake Baikal is unique as it is absolutely see-through and when you are walking on it you can see several metres down. Some people even experience vertigo. Baikal freezes relatively late and in February lots of ice-hummocks form – massive blocks of ice, some reaching heights of multi-storey buildings.",
+    Karelia:
+      'Karelia is a region in Northwestern Russia, known as the country of lakes. It has a strong cultural connection with Finland, with the ethnic Karelians closely related to the Finns. Much of the Finnish national epic Kalevala was collected here. Karelia is known as "the country of lakes." One quarter of Karelia`s surface is covered by water including about sixty thousand lakes. The second-largest lake of Europe, Lake Onega, is located in Karelia. The largest lake of Europe, Lake Ladoga, is partly located in Karelia (together with Leningrad Oblast). Wherever there is land, there are dense forests covering the ground.',
+  },
+  train_cover_title: 'To Baikal "with dog sledding"',
+  train_cover_parag:
+    "Based on the educational topic about the Transsib - a journey from the capital to Lake Baikal by train.",
+};
